83c7965bedd7407444aa0a0b844c8f9f86ed3d6d
JerryismyName <119637933+JerryismyName@users.noreply.github.com>
2022-12-01 21:29:15 -0800

    Initial
    Initial

---
+++ Day2/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(@"Press 1 or 2 for puzzle part: ");
+            var result = Console.ReadKey();
+            Console.WriteLine();
+
+            if (result.KeyChar == '1')
+            {
+                Part1();
+            }
+            else if (result.KeyChar == '2')
+            {
+                Part2();
+            }
+            else
+            {
+                Console.WriteLine(@"Incorrect key press");
+            }
+
+            Console.WriteLine(@"Press any key to quit.");
+            Console.ReadKey();
+        }
+    }
+}
